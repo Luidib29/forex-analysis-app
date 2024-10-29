15f0c801e14782a7dff4f259e381788514ca179c
Luidib29 <diblasiluigi0@gmail.com>
2024-10-30 00:38:30 +0100

    Update forex_app.py

--- forex_app.py
+++ forex_app.py
@@ -75,23 +75,23 @@ def analisi_forex(symbol, pair_name):
         macd = df['MACD'].iloc[i]
         signal = df['Signal'].iloc[i]
         
-        if rsi < 30:
-            if macd > signal:
-                df.loc[df.index[i], 'Segnale'] = 'COMPRA'
-            else:
-                df.loc[df.index[i], 'Segnale'] = 'ATTENDI (RSI ipervenduto ma MACD negativo)'
-        elif rsi > 70:
-            if macd < signal:
-                df.loc[df.index[i], 'Segnale'] = 'VENDI'
-            else:
-                df.loc[df.index[i], 'Segnale'] = 'ATTENDI (RSI ipercomprato ma MACD positivo)'
-        else:
-            if macd > signal and rsi > 40:
-                df.loc[df.index[i], 'Segnale'] = 'COMPRA'
-            elif macd < signal and rsi < 60:
-                df.loc[df.index[i], 'Segnale'] = 'VENDI'
-            else:
-                df.loc[df.index[i], 'Segnale'] = 'ATTENDI'
+       if rsi < 35:  # Aumentato da 30
+    if macd > signal:
+        df.loc[df.index[i], 'Segnale'] = 'COMPRA'
+    else:
+        df.loc[df.index[i], 'Segnale'] = 'ATTENDI (RSI ipervenduto ma MACD negativo)'
+elif rsi > 65:  # Diminuito da 70
+    if macd < signal:
+        df.loc[df.index[i], 'Segnale'] = 'VENDI'
+    else:
+        df.loc[df.index[i], 'Segnale'] = 'ATTENDI (RSI ipercomprato ma MACD positivo)'
+else:
+    if macd > signal and rsi > 35:  # Diminuito da 40
+        df.loc[df.index[i], 'Segnale'] = 'COMPRA'
+    elif macd < signal and rsi < 65:  # Aumentato da 60
+        df.loc[df.index[i], 'Segnale'] = 'VENDI'
+    else:
+        df.loc[df.index[i], 'Segnale'] = 'ATTENDI'
     
     return df
     # Analisi per ogni coppia
