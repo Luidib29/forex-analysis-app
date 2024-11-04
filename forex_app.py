4650f5fa5ce514f8d1ee0060d6718c26b26c1fcd
Luidib29 <diblasiluigi0@gmail.com>
2024-11-04 22:56:47 +0100

    Update forex_app.py

--- forex_app.py
+++ forex_app.py
@@ -10,6 +10,24 @@ import requests
 import re
 import json
 import os
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+
+# Configurazione dell'authenticator
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+# Configurazione pagina
+st.set_page_config(
 
 # Configurazione pagina
 st.set_page_config(
