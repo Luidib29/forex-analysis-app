8cf98cb98b540a48587962ae0aa8fdd3f86e8e46
Luidib29 <diblasiluigi0@gmail.com>
2024-11-04 23:27:57 +0100

    Update forex_app.py

--- forex_app.py
+++ forex_app.py
@@ -18,11 +18,10 @@ with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
+    credentials=config['credentials'],
+    cookie_name=config['cookie']['name'],
+    key=config['cookie']['key'],
+    cookie_expiry_days=config['cookie']['expiry_days']
 )
 
 # Configurazione pagina
